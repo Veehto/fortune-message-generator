d207a7a37e0c6fed6f174a0655d9df0663abd27b
Victor CS <cifusuil.victor@gmail.com>
2023-09-02 22:13:33 -0400

    Stores message components as arrays in an object.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+// Store phrases as arrays in an object.
+const fortuneRepository = {
+    goodFortune: ['An exciting opportunity lies ahead of you.', 'You will always be surrounded by true friends.', 
+    'You should be able to undertake and complete anything.', 'Respect yourself and others will respect you.', 
+    'Your life will be happy and peaceful.', 'The one you love is closer than you think.', 'Soon you will be sitting on top of the world.', 
+    'You will be rewarded for your efforts within the month.', 'A new work opportunity will avail itself.', 'Doors will open for you in many areas of your life.'],
+    badFortune: ['You will die alone and poorly dressed.', 'Today is not your day.', 'You will die of a heart attack.', 'Enjoy yourself while you can', 
+    'You have an illness you ought to check as soon as you can.', 'You will learn a lesson today.', 'Something unexpected will disrupt your day.', 
+    'You laugh now, wait until you get home.', 'You may have forgotten something.', 'The fact that others are bad does not imply you are good.'],
+    advForGoodFortune: ['take time to know yourself.', 'not make assumptions.', 'be patient and persistent.', 'trust your instincts.', 'not be afraid, take the first step now.'],
+    advForBadFortune: ['know when to walk away.', 'avoid confrontation.', 'not leave your house.', 'learn that you do not always get what you want.', 'use adversity as an opportunity.'],
+};
