d4418650283febacf9746092b2d21c597ecebb94
Victor CS <cifusuil.victor@gmail.com>
2023-09-02 22:23:32 -0400

    Adds functionality as object methods to randomly select message components: lucky number, good/bad fortune, and advice on them.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Returns a random number to select an array's index depending on array's length.
+const randNumber = (number) => {
+    return Math.floor(Math.random() * number);
+  };
+
 // Store phrases as arrays in an object.
 const fortuneRepository = {
     goodFortune: ['An exciting opportunity lies ahead of you.', 'You will always be surrounded by true friends.', 
@@ -9,4 +14,35 @@ const fortuneRepository = {
     'You laugh now, wait until you get home.', 'You may have forgotten something.', 'The fact that others are bad does not imply you are good.'],
     advForGoodFortune: ['take time to know yourself.', 'not make assumptions.', 'be patient and persistent.', 'trust your instincts.', 'not be afraid, take the first step now.'],
     advForBadFortune: ['know when to walk away.', 'avoid confrontation.', 'not leave your house.', 'learn that you do not always get what you want.', 'use adversity as an opportunity.'],
+    luckyNumber() {
+        return Math.ceil(Math.random() * 99);
+    },
+    checkFortune() {
+        const coinToss = Math.floor(Math.random() * 1);
+        
+        if (coinToss === 1) {
+            let randSelect = randNumber(this.goodFortune.length);
+            let goodOne = this.goodFortune[randSelect];
+            return goodOne;
+        } else {
+            let randSelect = randNumber(this.badFortune.length);
+            let badOne = this.badFortune[randSelect];
+            return badOne;
+        };
+    },
+    giveAdvice() {
+        const yourLuck = this.checkFortune();
+
+        if (yourLuck === this.goodOne) {
+            let randSelect = randNumber(this.advForGoodFortune.length);
+            let advice = this.advForGoodFortune[randSelect];
+            return advice;
+        } else {
+            let randSelect = randNumber(this.advForBadFortune.length);
+            let advice = this.advForBadFortune[randSelect];
+            return advice;
+        };        
+    }
 };
+
+
