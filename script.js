0590366f4c63e6e965ba9036b4fdffe7fb86a052
Victor CS <cifusuil.victor@gmail.com>
2023-09-06 22:51:54 -0300

    Corrects logic errors in RNG function, coin toss changes to a global const, corrects typo in furtuneRepository array.

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const randNumber = (number) => {
     return Math.floor(Math.random() * number);
   };
 
+// Toss a coin and save whether it is heads or tails (1 or 0).
+const coinToss = Math.floor(Math.random() * 2);
+
 // Store phrases as arrays in an object. Funcionality is applied as methods. To get fortune, just call the last method.
 const fortuneRepository = {
     goodFortune: ['An exciting opportunity lies ahead of you.', 'You will always be surrounded by true friends.', 
     'You should be able to undertake and complete anything.', 'Respect yourself and others will respect you.', 
     'Your life will be happy and peaceful.', 'The one you love is closer than you think.', 'Soon you will be sitting on top of the world.', 
     'You will be rewarded for your efforts within the month.', 'A new work opportunity will avail itself.', 'Doors will open for you in many areas of your life.'],
-    badFortune: ['You will die alone and poorly dressed.', 'Today is not your day.', 'You will die of a heart attack.', 'Enjoy yourself while you can', 
+    badFortune: ['You will die alone and poorly dressed.', 'Today is not your day.', 'You will die of a heart attack.', 'Enjoy yourself while you can.', 
     'You have an illness you ought to check as soon as you can.', 'You will learn a lesson today.', 'Something unexpected will disrupt your day.', 
     'You laugh now, wait until you get home.', 'You may have forgotten something.', 'The fact that others are bad does not imply you are good.'],
     advForGoodFortune: ['take time to know yourself.', 'not make assumptions.', 'be patient and persistent.', 'trust your instincts.', 'not be afraid, take the first step now.'],
@@ -18,9 +21,9 @@ const fortuneRepository = {
         return Math.ceil(Math.random() * 99);
     },
     checkFortune() {
-        const coinToss = Math.floor(Math.random() * 1);
+        const destiny = coinToss;
         
-        if (coinToss === 1) {
+        if (destiny === 1) {
             let randSelect = randNumber(this.goodFortune.length);
             let goodOne = this.goodFortune[randSelect];
             return goodOne;
@@ -31,9 +34,9 @@ const fortuneRepository = {
         };
     },
     giveAdvice() {
-        const yourLuck = this.checkFortune();
+        const yourLuck = coinToss;
 
-        if (yourLuck === this.goodOne) {
+        if (yourLuck === 1) {
             let randSelect = randNumber(this.advForGoodFortune.length);
             let advice = this.advForGoodFortune[randSelect];
             return advice;
